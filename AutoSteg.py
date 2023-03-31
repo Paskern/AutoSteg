81ef55c2ab60ecaab29f2b9faecbd86f0088b152
Paskern <StefanPakaski@gmail.com>
2023-03-31 18:46:46 +0200

    Fix filetype filter and logging

--- AutoSteg.py
+++ AutoSteg.py
@@ -7,6 +7,9 @@ import time
 import shutil
 import tempfile
 from modules import stegseek
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
 def get_image_links(url):
     response = requests.get(url)
@@ -35,29 +38,31 @@ def main(url, download_directory, interval):
         os.makedirs(download_directory)
     
     downloaded_images_hash = set()
+    allowed_file_types = ['.jpg', '.jpeg', '.bmp']
 
     while True:
         try:
             image_links = get_image_links(url)
             for link in image_links:
-                _, file_type = os.path.splitext(link)
-                if file_type.lower() in ['.jpg', '.jpeg', '.bmp']:
+                link = link.split("?")[0]
+                if any(file_type in link.lower() for file_type in allowed_file_types):
                     temp_path, file_name = download_image(link, download_directory)
                     md5_hash = md5_file(temp_path)
                     if md5_hash not in downloaded_images_hash:
                         downloaded_images_hash.add(md5_hash)
                         final_path = os.path.join(download_directory, file_name)
                         os.rename(temp_path, final_path)
-                        print(f"[+] Successfully downloaded {link}")
+                        logging.info(f"Succesfully downloaded {link}")
 
                         stegseek.crack_wordlist(final_path, "rockyou.txt")
                     else:
                         os.remove(temp_path)
         except Exception as e:
-            print(f"[!] Error while processing: {e}")
-        
-        print(f"[+] Sleeping for {interval} seconds")
+            logging.error(f"Error while processing: {e}")
+
+        logging.info(f"No new images, sleeping for {interval} seconds...")
         time.sleep(interval)
+
 
 if __name__ == "__main__":
     url = "https://vg.no"
